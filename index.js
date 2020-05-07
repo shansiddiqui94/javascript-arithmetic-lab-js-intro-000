f63a0005c2c398ca3f4b85c499470530f02ffdd8
Shan Siddiqui <shansiddiqui94@gmail.com>
2020-05-07 18:29:34 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,40 @@
+function add(){
+var number = 2
+  number + 5
+}
+
+function add(a, b){
+   return a + b
+}
+
+function subtract(a, b){
+  return a - b
+}
+
+function multiply(a, b) {
+  return a * b
+  }
+
+function divide(a, b) {
+  return a / b
+}
+
+function increment(a){
+    return a + 1
+}
+
+function increment(a){
+    return a+1
+}
+
+function decrement(a){
+    return a-1
+}
+
+function makeInt (n){
+  return parseInt(n, 10)
+}
+
+function preserveDecimal(n){
+  return parseFloat(n)
+}
